a2082a1b314d8c0ff37e6558b69c041934ba7083
Piyush <dpiyush28@gmail.com>
2025-07-17 12:02:59 +0530

    Update-Product error fix

--- HeartsDesireLuxury.Core/DTO/ProductUpdateRequest.cs
+++ HeartsDesireLuxury.Core/DTO/ProductUpdateRequest.cs
@@ -1,27 +1,40 @@
 ﻿using Entities;
-using System;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ServiceContracts.DTO
 {
     public class ProductUpdateRequest
     {
-        [Required(ErrorMessage ="ProductID is required to update any product")]
+        [Required(ErrorMessage = "ProductID is required to update any product")]
         public int? ProductID { get; set; }
+
         public double ProductPrice { get; set; }
         public double? ProductSalePrice { get; set; }
         public int Stock { get; set; }
         public string? SkuID { get; set; }
+
         public string? MainImagePath { get; set; }
         public List<string>? ReferenceImagePaths { get; set; }
+
+        public IFormFile? MainImage { get; set; }
+        public List<IFormFile>? ReferenceImages { get; set; }
+
         public int? CategoryID { get; set; }
+
         public Products ToProduct()
         {
-            return new Products() { ProductID = ProductID, ProductPrice = ProductPrice, ProductSalePrice = ProductSalePrice, Stock = Stock, SkuID = SkuID,CategoryID = CategoryID,MainImagePath = MainImagePath};
+            return new Products()
+            {
+                ProductID = ProductID ?? 0,
+                ProductPrice = ProductPrice,
+                ProductSalePrice = ProductSalePrice,
+                Stock = Stock,
+                SkuID = SkuID,
+                MainImagePath = MainImagePath,
+                ReferenceImages = ReferenceImagePaths != null ? string.Join("\n", ReferenceImagePaths) : null,
+                CategoryID = CategoryID
+            };
         }
     }
 }
